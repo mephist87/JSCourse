74de95aa7dcf318e684302e9294b52b300163f77
u_m0vb5 <svkrasnov@alfabank.ru>
2017-10-19 20:15:30 +0300

    homework3 encoder

---
+++ Module3/src/encoder.js
@@ -0,0 +1,13 @@
+class SubstitutionCipher {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    encode(str) {
+        let result = '';
+        [...str].forEach(c => result += this.b.charAt(this.a.indexOf(c)));
+        return result;
+    }
+
+}
