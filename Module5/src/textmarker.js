b0f4a0d33e888f657b22988ac8b4500e5e9f4516
u_m0vb5 <svkrasnov@alfabank.ru>
2017-11-03 19:05:39 +0300

    homework 5

--- Module5/src/textmarker.js
+++ Module5/src/textmarker.js
@@ -1,10 +1,23 @@
 export function initTextMarkers() {
     let buttons = document.querySelectorAll('.btn');
-    let tooltipItems = document.querySelectorAll('.tooltip-item');
 
     buttons.forEach(btn => {
+        let isTooltipHidden = btn.innerHTML === 'Показать';
         btn.addEventListener('click', () => {
-            btn.innerHTML = (el.innerHTML === 'Свернуть' ? 'Показать' : 'Свернуть');
+            if (isTooltipHidden) {
+                btn.innerHTML = 'Скрыть';
+                isTooltipHidden = false;
+                btn.parentNode.querySelectorAll('[data-tooltip]').forEach((tc) => {
+                    let tooltip = document.createElement('span');
+                    tooltip.classList.add('tooltip-item');
+                    tooltip.innerHTML = tc.getAttribute('data-tooltip');
+                    tc.appendChild(tooltip);
+                });
+            } else {
+                isTooltipHidden = true;
+                btn.innerHTML = 'Показать';
+                btn.parentNode.querySelectorAll('.tooltip-item').forEach(e => e.parentNode.removeChild(e));
+            }
         })
     })
 
--- Module5/webpack.config.js
+++ Module5/webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path'); // забираем стандартный нодовый модуль path
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
+
+const buildPath = path.resolve(__dirname, '.build');
 
 module.exports = {
     entry: './src/index.js',
+
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '.build') // _dirname - глобальная, указывает на текущую директорию
+        path: buildPath // _dirname - глобальная, указывает на текущую директорию
     },
     module: {
         rules: [{
@@ -16,5 +20,8 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(buildPath)
+]
 };
